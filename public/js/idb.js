7982b10c8dd5e769bbb0ae4909145949177486c4
sarahwylie <sarahannemariawylie@gmail.com>
2022-03-17 16:54:48 -0500

    added indexdb

---
+++ public/js/idb.js
@@ -0,0 +1,23 @@
+let db;
+const request = indexedDB.open('cache-ching', 1);
+
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    db.createObjectStore('new_transaction', { autoIncrement: true });
+};
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+    if (navigator.onLine) {
+    }
+};
+
+request.onerror = function(event) {
+    console.log(event.target.errorCode);
+  };
+
+function saveRecord(record) {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+    transactionObjectStore.add(record);
+}
