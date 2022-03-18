60a0d817dbbac88c581dc5b09a0ee1d06546d481
sarahwylie <sarahannemariawylie@gmail.com>
2022-03-17 19:17:15 -0500

    woot service workers!

--- public/js/idb.js
+++ public/js/idb.js
@@ -9,6 +9,7 @@ request.onupgradeneeded = function(event) {
 request.onsuccess = function(event) {
     db = event.target.result;
     if (navigator.onLine) {
+        uploadTransaction();
     }
 };
 
@@ -21,3 +22,35 @@ function saveRecord(record) {
     const transactionObjectStore = transaction.objectStore('new_transaction');
     transactionObjectStore.add(record);
 }
+
+function uploadTransaction() {
+    const transaction = db.transaction(['new-transaction'], 'readwrite');
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+    const getAll = transactionObjectStore.getAll();
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if (serverResponse.message) {
+                    throw new Error(serverResponse);
+                }
+                const transaction = db.transaction(['new_transaction'], 'readwrite');
+                const transactionObjectStore = transaction.objectStore('new_transaction');
+                transactionObjectStore.clear();
+                alert('Your saved budget items have been updated!')
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        }
+    }
+}
+window.addEventListener('online', uploadTransaction);
